c15610f894949276237442179f02eb2cf2748ed4
Bruna Xavier <brunabxsantos@gmail.com>
2015-02-11 12:31:15 -0200

    Configurações de teste
    - Adicionando ao Gruntfile o Jasmine

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,11 +2,24 @@ module.exports = function(grunt) {
   grunt.initConfig({
     jshint: {
       all: ['Gruntfile.js', 'test/*.js', 'public/js/*.js']
+    },
+    jasmine : {
+      components : {
+        src : 'public/js/controllers.js',
+        options : {
+          specs : 'test/*Spec.js',
+          vendor : 'public/js/lib/*.js',
+          helpers : 'test/lib/*.js',
+          keepRunner : true
+        }
+      }
     }
   });
 
   grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-jasmine');
+
 
   // Default task.
-  grunt.registerTask('travis', ['jshint']);
+  grunt.registerTask('travis', ['jshint', 'jasmine']);
 };
