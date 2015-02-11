8f0562cdbd73d70cc7e6ba3b7039896c53c0dfa9
Bruna Xavier <brunabxsantos@gmail.com>
2015-02-11 13:14:19 -0200

    Adicionando funções à Animals
    - cssSelector
    - style
    - domElement
    - appendToDOM

--- public/js/animals.js
+++ public/js/animals.js
@@ -1,5 +1,29 @@
 var Animals = Animals || {};
 
+Animals.cssSelector = function(animal) {
+  return '#' + animal.id;
+};
+
+Animals.style = function(size, position) {
+  return 'position:absolute;'
+       + 'width:' + size.width + ';'
+       + 'height:' + size.height + ';'
+       + 'top:' + position.y + ';'
+       + 'left:' + position.x + ';';
+};
+
+Animals.domElement = function(animal) {
+  return '<div'
+       + ' id="' + animal.id + '"'
+       + ' class="' + animal.class + '"'
+       + ' style="' + Animals.style(animal.size, animal.position) + '"'
+       + '></div>';
+};
+
+Animals.appendToDOM = function(animal) {
+  jQuery('#game-background').append(Animals.domElement(animal));
+};
+
 Animals.hen = function(x, y) {
   var self = this;
 
--- test/AnimalsSpec.js
+++ test/AnimalsSpec.js
@@ -29,3 +29,69 @@ describe('hen object initialization', function() {
     expect(Animals.count).toBe(1);
   });
 });
+
+describe('cssSelector function', function() {
+  beforeEach(function() {
+    Animals.count = 0;
+  });
+
+  it('must return animal css selector based on animal\'s instance\'s id attribute', function() {
+    // Arrange
+    var hen = new Animals.hen(0, 0);
+
+    // Act
+    var cssSelector = Animals.cssSelector(hen);
+
+    // Assert
+    expect(cssSelector).toBe('#game-hen-1');
+  });
+});
+
+describe('style function', function() {
+  it('must return style based on position and size', function() {
+    // Arrange
+    var size = {width: '12px', height: '13px'};
+    var position = {x: 14, y: 15};
+
+    // Act
+    var style = Animals.style(size, position);
+
+    // Assert
+    expect(style).toBe('position:absolute;width:12px;height:13px;top:15;left:14;');
+  });
+});
+
+describe('domElement function', function() {
+  beforeEach(function() {
+    Animals.count = 0;
+  });
+
+  it('must return HTML div based with attributes based on animal\'s instance\'s attributes', function() {
+    // Arrange
+    var hen = new Animals.hen(14, 15);
+
+    // Act
+    var domElement = Animals.domElement(hen);
+
+    // Assert
+    expect(domElement).toBe('<div id="game-hen-1" class="game-animal game-hen" style="position:absolute;width:32px;height:32px;top:15;left:14;"></div>');
+  });
+});
+
+describe('appendToDOM function', function() {
+  beforeEach(function() {
+    Animals.count = 0;
+  });
+
+  it('must be called with domElement function\'s result', function() {
+    // Arrange
+    var spy = spyOn($.fn, 'append');
+    var hen = new Animals.hen(14, 15);
+
+    // Act
+    Animals.appendToDOM(hen);
+
+    // Assert
+    expect(spy).toHaveBeenCalledWith('<div id="game-hen-1" class="game-animal game-hen" style="position:absolute;width:32px;height:32px;top:15;left:14;"></div>');
+  });
+});
